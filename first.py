5362227e23412c93ba71fd29a6d0d327a681fb11
sherzod202021 <72793885+sherzod202021@users.noreply.github.com>
2021-06-09 09:36:42 +0500

    added first changes to the first.py

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
 matn = "Bugun havo juda ajoyib hammma ishlarni qilishga zo'r sheroit bo'ldi"
 print("hamma" in (matn))
+matnn = "Bugun havo juda issiq lekin issiq ham ham yaxshi"
+if "lekint" in matnn:
+	print("To'g'ri topdingiz ushbu matnda lekin so'zi mavjud")
+else:
+	print("Ushbu matnda lekin so'zi mavjud emas")
+		
