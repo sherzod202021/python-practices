dc3ada8cf18ceb4f8ac46a788d956ec117e5a104
sherzod202021 <72793885+sherzod202021@users.noreply.github.com>
2021-06-09 10:59:01 +0500

    changes first.py

--- first.py
+++ first.py
@@ -1,5 +1,12 @@
+
+                #First practice
+
 matn = "Bugun havo juda ajoyib hammma ishlarni qilishga zo'r sheroit bo'ldi"
 print("hamma" in (matn))
+
+
+                #Second practice
+
 matnn = "Bugun havo juda issiq lekin issiq ham ham yaxshi"
 if "lekint" in matnn:
 	print("To'g'ri topdingiz ushbu matnda lekin so'zi mavjud")
@@ -7,6 +14,7 @@ else:
 	print("Ushbu matnda lekin so'zi mavjud emas")
 
 
+                 #Third practice
 text = "Surxondaryo viloyati Sariosiyo tumani IT park dasturchilari bugun bitiruvchi sertifikatini olishdi"
 if "IT park dasturchilarii" in text:
 	print("Ushbu matnda 'IT park dasturchilari' sozi mavjud")
@@ -14,15 +22,26 @@ else:
 	print("Ushbu matnda 'IT park dasturchilari' sozi mavjud emas")
 
 
+               #Fourth practice
+	
 text1 = "Bugun IT park dasturchilarini sertifikat oladigan kuni"
 print("sertifikatlar" not in text1)
 print("sertifikatlar" in text1)
 
 
-
+                #Fiveth practice
 
 text2 = "Bugun IT park dasturchilari uchun omadli kun chunki bugun sertifikat olishiyapti"
 print(text2[2:12])
 print(text2[-12:-2])
 print(text2[:8])
+
+
+                #Sixth practice
+
+text3 = "Bugun Surxondaryo viloyati xokimi Sariosiyo tumaniga keladi"
+print(text3.upper())
+
+
+
 		
